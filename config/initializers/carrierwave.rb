158638d174643f17f61061898138b93abb3b4eb0
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-18 23:07:16 +0900

    テスト

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,21 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       # Amazon S3用の設定
-#       provider: 'AWS',
-#       region: ENV['AWS_S3_REGION'],
-#       aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-#       aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
-#     }
-#     config.fog_directory     =  ENV['AWS_S3_BUCKET']
-#     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
-#   end
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      provider: 'AWS',
+      region: 'videotube-28689',
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+    }
+    config.fog_directory     =  'videotube-28689'
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
 
-#   # 日本語ファイル名の設定
-#   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
-# end
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+end
